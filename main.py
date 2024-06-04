26ca36a889a2883cf8297170646c26f5e9410561
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-06-04 20:21:06 +0300

    Update main.py
    Stable requast code

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import requests
+import json
+Response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json") 
+jsonString = Response.text
+result = json.loads(jsonString)
 
-url = "https://livescore-api.com/api-client/matches/lineups.json?match_id=194137&key=demo_key&secret=demo_secret        "
 
-querystring = {"page":"2"}
-
-headers = {
-	"x-rapidapi-key": "8b33fc0c1fmsh1e35e80b5af5be8p1f5100jsn188f58afd56c",
-	"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
+print(result["bpi"]["USD"]["rate"])
